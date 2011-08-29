b005ecf55dd8273676cadc6d21f6ef28a111fdee
laxmi.thebe@gmail.com <laxmi.thebe@gmail.com@99aaf835-898d-60b9-e47b-b85249bbc93e>
2011-08-29 10:04:28 +0000

    First Test

---  codecamp-timereporter --username martin.koskinen@gmail.com/TimeReporting/Controllers/HomeController.cs
+++  codecamp-timereporter --username martin.koskinen@gmail.com/TimeReporting/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace TimeReporting.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Welcome to Group G Time controller!";
 
             return View();
         }
