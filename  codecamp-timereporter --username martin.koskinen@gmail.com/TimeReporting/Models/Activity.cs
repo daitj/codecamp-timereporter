d9785e68e2ae998e54d07c39eba96c54f95ca52f
oskar.ostman@gmail.com <oskar.ostman@gmail.com@99aaf835-898d-60b9-e47b-b85249bbc93e>
2011-08-30 07:02:44 +0000

    some new classes and pure awesomeness

---
+++  codecamp-timereporter --username martin.koskinen@gmail.com/TimeReporting/Models/Activity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TimeReporting.Models
+{
+    public class Activity
+    {
+        public int id { get; private set; }
+        public string projectId { get; set; }
+        public string title { get; set; }
+    }
+}
