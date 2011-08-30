4011573d24725f82be9b7064a1a606f5a921b6a7
martin.koskinen@gmail.com <martin.koskinen@gmail.com@99aaf835-898d-60b9-e47b-b85249bbc93e>
2011-08-30 12:18:13 +0000

    Recreate database on model changes

---  codecamp-timereporter --username martin.koskinen@gmail.com/TimeReporting/Models/TimeReportingDataBaseEntities.cs
+++  codecamp-timereporter --username martin.koskinen@gmail.com/TimeReporting/Models/TimeReportingDataBaseEntities.cs
@@ -11,4 +11,17 @@ namespace TimeReporting.Models
         public DbSet<Activity> Activities { get; set; }
         public DbSet<Project> Projects { get; set; }
     }
+
+    public class TimeDataBaseInitializer : DropCreateDatabaseIfModelChanges<TimeReportingDataBaseEntities>
+    {
+        protected override void Seed(TimeReportingDataBaseEntities context)
+        {
+            List<string> names = new List<string>();
+            names.Add("Oka");
+            names.Add("Martin");
+            var projects = new Project() { title = "Dricka kaffe", userNames = names };
+            context.Projects.Add(projects);
+            context.SaveChanges();
+        }
+    }
 }
