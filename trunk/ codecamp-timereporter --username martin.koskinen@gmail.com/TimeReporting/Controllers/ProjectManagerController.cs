37171f992bf69f1a0a800eb3d532b121a449198c
karvinen.petteri@gmail.com <karvinen.petteri@gmail.com@99aaf835-898d-60b9-e47b-b85249bbc93e>
2011-08-30 09:31:01 +0000

    added ProjectManager

---
+++ trunk/ codecamp-timereporter --username martin.koskinen@gmail.com/TimeReporting/Controllers/ProjectManagerController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TimeReporting.Models;
+
+namespace TimeReporting.Controllers
+{ 
+    [Authorize(Roles="ProjectManager")]
+    public class ProjectManagerController : Controller
+    {
+        private ProjectDBContext db = new ProjectDBContext();
+
+        //
+        // GET: /ProjectManager/
+
+        public ViewResult Index()
+        {
+            return View(db.Projects.ToList());
+        }
+
+        //
+        // GET: /ProjectManager/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Project project = db.Projects.Find(id);
+            return View(project);
+        }
+
+        //
+        // GET: /ProjectManager/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /ProjectManager/Create
+
+        [HttpPost]
+        public ActionResult Create(Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Projects.Add(project);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(project);
+        }
+        
+        //
+        // GET: /ProjectManager/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Project project = db.Projects.Find(id);
+            return View(project);
+        }
+
+        //
+        // POST: /ProjectManager/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(project).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(project);
+        }
+
+        //
+        // GET: /ProjectManager/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Project project = db.Projects.Find(id);
+            return View(project);
+        }
+
+        //
+        // POST: /ProjectManager/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Project project = db.Projects.Find(id);
+            db.Projects.Remove(project);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ trunk/ codecamp-timereporter --username martin.koskinen@gmail.com/TimeReporting/Models/TimereportingEntities.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+namespace TimeReporting.Models
+{
+    public class TimereportingEntities : DbContext
+    {
+        public DbSet<Project> Project { get; set; }
+        public DbSet<Activity> Activity { get; set; }
+    }
+}
